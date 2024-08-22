adc3f2b2d1513482fb0b7de62e8cfdc713819a5a
Yoan <yoan.principal@gmail.com>
2024-08-22 14:34:10 +0200

    Ajout de l'importation du css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logoKasa from '../images/LOGO.png'
 
 const Header = () => (
   <header>
-    <nav>
+    <nav className = "main__menu">
+      <img src={logoKasa} alt = "Logo Kasa"/>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import '../styles/style.css';
+
+
 
 const MainLayout = ({ children }) => (
   <div className="main-layout">
