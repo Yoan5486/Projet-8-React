811337793322238430fe83993d9a541ac7541106
Yoan <yoan.principal@gmail.com>
2024-12-10 12:47:05 +0100

    Fluidité enntre les dimensions responsives de la page d'acceuil

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import backgroundHome from '../images/background_home.jpeg'
+import backgroundLogement from '../images/image_logement.png'
 
 
 const Home = () => {
@@ -19,9 +20,10 @@ return (
             <img className = "img__background--home" src={backgroundHome} alt="Arrière plan page d'accueil"/>
             <h2 className = "title__home">Chez vous, partout et ailleurs</h2>
         </div>
-        <div className='cards'>
+        <div className="cards">
         {cards.map((_, index) => (
           <div className="card" key={index} onClick={() => handleCardClick('/details')}>
+            <img className="img__logement" src={backgroundLogement} alt = "Logement en arrière plan des cards"/>
             <p className="title__card">Titre de la<br />
             location
             </p>
