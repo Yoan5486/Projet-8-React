94cbbffceeeb92fecfdff179485846e4f8e6d93a
Yoan <yoan.principal@gmail.com>
2024-12-11 11:28:57 +0100

    Ajout de la page d'erreur

--- src/pages/error404.jsx
+++ src/pages/error404.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Error404 = () => <h2>404 - Page Not Found</h2>;
+const Error404 = () => {
+
+return (
+        <div>
+            <h1 className = "error__title">404</h1>;
+            <h2 className ="title__notfound">Oups! La page que vous demandez n'existe pas.</h2>   
+            <Link to="/" className="link__home--error">Retourner sur la page d'accueil</Link>
+        </div>
+)
+    }
 
 export default Error404;
