5e902caad5139f5f002e30270ae60a260dd511f9
Yoan <yoan.principal@gmail.com>
2024-12-14 19:47:58 +0100

    Slideshow créer non responsive

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -2,36 +2,48 @@ import React, { useState } from "react";
 
 const Slideshow = ({ pictures }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
+  const [fade, setFade] = useState(false)
 
   const handleNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
-    )
+    setFade(true)
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
+      )
+      setFade(false)
+    }, 300) 
   }
 
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
-    )
+    setFade(true)
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
+      )
+      setFade(false)
+    }, 300)
   }
 
   return (
     <div className = "slideshow">
-      <div className = "slideshow__img--container">
-        <img src = {pictures[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="slideshow__img"/></div>
+      <div className = {`slideshow__img--container ${fade ? "fade" : ""}`}>
+        <img src = {pictures[currentIndex]} alt={`Slide ${currentIndex + 1}`} className = "slideshow__img"/>
+        </div>
       {pictures.length > 1 && (
         <>
-          <button className="slideshow__arrow slideshow__arrow--left" onClick={handlePrev}>
+          <button className = "slideshow__arrow slideshow__arrow--left" onClick={handlePrev}>
             &#10094;
           </button>
-          <button className="slideshow__arrow slideshow__arrow--right" onClick={handleNext}>
+          <button className = "slideshow__arrow slideshow__arrow--right" onClick={handleNext}>
             &#10095;
           </button>
         </>
       )}
-      <div className="slideshow__counter">
-        {currentIndex + 1} / {pictures.length}
-      </div>
+     {pictures.length > 1 && (
+        <div className = "slideshow__counter">
+          {currentIndex + 1} / {pictures.length}
+        </div>
+     )}
     </div>
   )
 }
